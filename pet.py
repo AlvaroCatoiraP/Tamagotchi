8d0d5e543211f1280636a2b7b5a502997e0e2f4d
AlvaroCatoira <acatoira@student.hers.be>
2026-03-22 23:41:12 +0100

    comienzo de librerias

--- pet.py
+++ pet.py
@@ -16,13 +16,59 @@ def constructor(name : str) -> dict:
         "hungry": 100, # 0 = no hungry - 100 = very hungry
         "happynes" : 100 # 0 sad - 100 happy
     }
+    save__update_pet(pet)
     return pet
 
-def save__update_Pet(pet:dict) -> bool:
+def save__update_pet(pet:dict) -> bool:
     
     try:
-        file = open(f"{pet["name"]}.data", "w")
-        dump = json.dump(pet)
-        json.load(file,dump)
+        file = open(f"{pet["name"]}.json", "w")
+        json.dump(pet, file, indent=4)
     except:
         print("Un problema surgio al momento de abrir el archivo de datos del Tamagotchi")
+    finally:
+        file.close()
+
+def load_pet() -> dict:
+        try:
+            with open(f"{name}.json", "r") as file:
+                pet = json.load(file)
+                return pet
+        except Exception as e:
+            print("Un problema surgio al momento de abrir el archivo de datos del Tamagotchi")
+
+def minus_attr(minus :int, name:str, attr : str):
+
+    try:
+        with open(f"{name}.json", "r") as file:
+            pet = json.load(file)
+            if pet[attr] - minus >= 0  :
+                pet[attr] = pet[attr] - minus
+            
+        with open(f"{name}.json", "w") as file:
+            json.dump(pet,file,indent=4)
+
+    except Exception as e:
+        print("Un problema surgio al momento de abrir el archivo de datos del Tamagotchi")
+    except KeyError:
+        print("Atributo inexistente en los datos del tamagotchi")
+
+
+def plus_attr(plus : int, name: str, attr : str):
+
+    try:
+        with open(f"{name}.json", "r") as file:
+            pet = json.load(file)
+            if pet[attr] + plus <= 100:
+                pet[attr] = pet[attr] + plus
+            
+        with open(f"{name}.json", "w") as file:
+            json.dump(pet,file,indent=4)
+
+    except Exception as e:
+        print("Un problema surgio al momento de abrir el archivo de datos del Tamagotchi")
+    except KeyError:
+        print("Atributo inexistente en los datos del tamagotchi")
+
+
+pet = constructor("toto")
